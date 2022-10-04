165d7e005a4f91bc64ca561d3579efcf3edf07ab
framnz85 <clavmall.85@gmail.com>
2022-10-04 22:15:28 +0800

    commit 10-4-22 11:30 pm

--- routes/ogpa.js
+++ routes/ogpa.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { getOgpa, getOgpaEmail, existOgpa, newOgpa, manyChat, manyChatPurchase } = require("../controllers/ogpa");
 
 router.get("/ogpa", getOgpa);
-router.get("/ogpa/:email", getOgpaEmail);
-router.get("/ogpa/:email/:password", existOgpa);
+router.get("/ogpa-email/:email", getOgpaEmail);
+router.get("/ogpa-password/:email/:password", existOgpa);
 router.post("/ogpa/new", newOgpa);
 
 router.get("/manychat/:mcid/:flowns", manyChat);
