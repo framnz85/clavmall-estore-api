dfe057bdfbd7859d3b006e58ee9d5bf1fa41dfe3
framnz85 <clavmall.85@gmail.com>
2022-10-22 16:19:35 +0800

    commit 10-22-22 4:30 pm

---
+++ controllers/export.js
@@ -0,0 +1,32 @@
+const ObjectId = require("mongoose").Types.ObjectId;
+const Product = require("../models/product");
+
+exports.allproducts = async (req, res) => {
+  const estoreid = req.headers.estoreid;
+  try {
+    const products = await Product(estoreid).find({}, " _id title supplierPrice markup markuptype ")
+      .sort({ title: 1 })
+      .exec();
+
+    res.json(products);
+  } catch (error) {
+    res.status(400).send("Export all products failed");
+  }
+};
+
+exports.importProducts = async (req, res) => {
+  const estoreid = req.headers.estoreid;
+  try {
+    const products = req.body.products;
+    for (let i = 0; i < products.length; i++) {
+      await Product(estoreid).findOneAndUpdate(
+        { _id: ObjectId(products[i]._id) },
+        products[i],
+        { new: true }
+      );
+    }
+    res.json({ ok: true });
+  } catch (error) {
+    res.status(400).send("Importing products failed");
+  }
+};
